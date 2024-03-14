4a0cd5f1e7029acd7a5b8c18a130f90da5429f76
DylSchaf <156049992+DylSchaf@users.noreply.github.com>
2024-03-14 19:33:26 -0400

    Update pandas.sh

--- pandas.sh
+++ pandas.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sed '/Zoo/d' pandas.csv > pandas_zoos.txt
+sed '/[Zz][Oo][Oo]/d' pandas.csv > pandas_zoos.txt
 awk -v FS=, '{print $1,$4,$2}' pandas.csv | sort -k3 > sorted_pandas.txt
 sort -t, -k5 pandas.csv > pandas_zoo_A2Z.txt
 sed 's/female/F/g; s/male/M/g' pandas.csv > panda_m_f.txt
 grep -c 'female.*October' pandas.csv
-grep -n 'a' pandas.csv > panda_names.txt
+grep -n '[a-z]' pandas.csv > panda_names.txt
 sed -n -e '1p' -e '6p' -e '17p' pandas.csv > lines_1_6_17.txt
 grep -i 'x' pandas.csv > x_lines.txt
 awk -v FS=, '{print $5}' pandas.csv | sort -u > unique_places.txt
