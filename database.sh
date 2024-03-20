f8b884808a00b0bfb6e68b84fb45cf944158c236
DylSchaf <156049992+DylSchaf@users.noreply.github.com>
2024-03-19 21:20:44 -0400

    Update database.sh

--- database.sh
+++ database.sh
@@ -3,7 +3,7 @@ makeblastdb -in ~/Desktop/mystery_transcripts.fasta -dbtype nucl -out ~/Desktop/
 makeblastdb -in ~/Desktop/mystery_proteins.fasta -dbtype prot -out ~/Desktop/mystery_proteins_db
 blastn -query ~/Desktop/mystery_nucleotide_query.fasta -db ~/Desktop/mystery_transcripts_db -evalue 1e-3 -outfmt 7 -max_target_seqs 5 -out ~/Desktop/nucleotides.out
 T010027234997, T010003453160, T010027234995
-grep -n 'T0' ~/Desktop/nucleotides.out | awk '{print $2}' > ~/Desktop/most_similar.txt
+grep -n 'T0' ~/Desktop/nucleotides.out | awk '$3 > 90 {print $2,$3}' > ~/Desktop/most_similar.txt
 blastp -query ~/Desktop/mystery_protein_query.fasta -db ~/Desktop/mystery_proteins_db -evalue 1e-5 -outfmt 7 -max_target_seqs 15 -out ~/Desktop/proteins.out
 P010027090796, P010027090798, P010027073865, P010027077695
 The species from the mystery_transcripts.fasta is Coffea arabica
